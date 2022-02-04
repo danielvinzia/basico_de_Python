1e08148325709700a47a803fc8d67eac237b0d58
danielvinzia <danielvinzia@gmail.com>
2022-02-04 11:42:35 -0300

    clase de variables lista

---
+++ conceptos_basicos_ de_python/02-variables.py
@@ -0,0 +1,15 @@
+# crear una variable
+numero = 3
+# inprime el valor de numero
+print(numero)
+# sumo dos variable
+numero1 = 5
+numero2 = 6
+numero1 + numero2
+11
+# cambio el valor de una variable ya asignada
+numero1 = 12
+# y si vuelvo a sumar da distitnto
+# sumo dos variables en otra
+resultado = numero1 + numero2
+

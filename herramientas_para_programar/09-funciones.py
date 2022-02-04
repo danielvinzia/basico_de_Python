ce9d90272ac802e727ee18794d6e3c245ca76ea3
danielvinzia <danielvinzia@gmail.com>
2022-02-04 14:33:17 -0300

    clase de funciones lista de verdad

--- herramientas_para_programar/09-funciones.py
+++ herramientas_para_programar/09-funciones.py
@@ -1,3 +1,4 @@
+# EJEMPLO 1
 # def imprimir_mensaje():
 #     print("Mensaje especial: ")
 #     print("¡Estoy aprendiendo a usar funciones!")
@@ -6,18 +7,28 @@
 # imprimir_mensaje()
 # imprimir_mensaje()
 
-def conversacion(mensaje):
-    print("Hola")
-    print("Como estas")
-    print(mensaje)
-    print("Adios")
+# def conversacion(mensaje):
+#     print("Hola")
+#     print("Como estas")
+#     print(mensaje)
+#     print("Adios")
 
-opcion = int(input("Elige una opcion (1, 2, 3):"))
-if opcion == 1:
-    conversacion("Elegiste la opcion 1")
-elif opcion == 2:
-    conversacion("Elegiste la opcion 2")
-elif opcion == 3:
-    conversacion("Elegiste la opcion 3")
-else:
-    print("Escribe la opcion correcta")
+# EJEMPLO 2
+# opcion = int(input("Elige una opcion (1, 2, 3):"))
+# if opcion == 1:
+#     conversacion("Elegiste la opcion 1")
+# elif opcion == 2:
+#     conversacion("Elegiste la opcion 2")
+# elif opcion == 3:
+#     conversacion("Elegiste la opcion 3")
+# else:
+#     print("Escribe la opcion correcta")
+
+# EJEMPLO 3
+def suma(a , b):
+    print("Se suman dos numeros")
+    resultado = a + b
+    return resultado
+
+sumatoria = suma(1, 4)
+print(sumatoria)

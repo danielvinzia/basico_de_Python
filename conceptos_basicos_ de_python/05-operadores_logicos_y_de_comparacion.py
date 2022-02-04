2851e040b7c18857b9b2406735b4ddb7ce8569f2
danielvinzia <danielvinzia@gmail.com>
2022-02-04 12:18:30 -0300

    clase de operadores logicos y de comparacion

---
+++ conceptos_basicos_ de_python/05-operadores_logicos_y_de_comparacion.py
@@ -0,0 +1,42 @@
+# creando booleanos
+es_estudiante = True
+es_estudiante
+True
+trabaja = False
+trabaja
+False
+# creo un and
+es_estudiante and trabaja
+False
+# creo un or
+es_estudiante or trabaja
+True
+# niego un booleano
+not es_estudiante
+False
+not trabaja
+True
+# consulto igualdad con el ==
+numero1 = 5
+numero2 = 5
+numero1
+5
+numero2
+5
+numero1 == numero2
+True
+numero3 = 7
+numero1 == numero3
+False
+# consulto si son distintos con !=
+numero1 != numero3
+True
+# comparo mayor, menor, mayor igual y menor igual
+numero1 > numero3
+False
+numero1 < numero3
+True
+numero1 >= numero3
+False
+numero1 <= numero3
+True

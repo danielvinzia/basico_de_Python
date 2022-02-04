7e9b25f47fda7cc9e86504bae77612e98d675c69
danielvinzia <danielvinzia@gmail.com>
2022-02-04 12:04:53 -0300

    clase de convertir un dato

---
+++ conceptos_basicos_ de_python/04-convertir_un_dato.py
@@ -0,0 +1,30 @@
+# mando un mensaje y guardo lo que el usuario escribe en una variable
+numero = input("Escribe un numero: ")
+#Escribe un numero: 4
+numero
+'4'
+numero2 = input("Escribe un numero: ")
+#Escribe un numero: 5
+numero2
+'5'
+# los guardo como caracteres por eso los concatena y no los suma
+numero + numero2
+'45'
+# tranformo mi numero de caracter a un entero
+numero = int(numero)
+numero
+4
+numero2 = int(numero2)
+numero2
+5
+numero + numero2
+9
+# paso a entero un numero decimal
+numero_decimal = 4.5
+int(4.5)
+4
+# convierto en caracter un numero decimal
+numero_decimal
+4.5
+str(numero_decimal)
+'4.5'

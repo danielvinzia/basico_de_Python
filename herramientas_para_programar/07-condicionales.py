623e154edc3ef464f71d6eeb18346ece58ccb7a0
danielvinzia <danielvinzia@gmail.com>
2022-02-04 14:00:19 -0300

    clase de condicionales lista

---
+++ herramientas_para_programar/07-condicionales.py
@@ -0,0 +1,16 @@
+# EJEMPLO 1
+# edad = int(input("escribe tu edad: "))
+# if edad > 17:
+#     print("Eres mayor de edad")
+# else:
+#     print("Eres menor de edad")
+
+# EJEMPLO 2
+numero = int(input("Escribe un numero: "))
+
+if numero > 5:
+    print("Es mayor a 5")
+elif numero == 5 :
+    print("Es igual a 5")
+else:
+    print("es menor a 5")

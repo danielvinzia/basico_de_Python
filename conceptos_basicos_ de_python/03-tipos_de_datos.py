b78c848a0ad944cb7cc6ed353a17adcbd1c1e10b
danielvinzia <danielvinzia@gmail.com>
2022-02-04 11:54:11 -0300

    clase de tipo de datos

---
+++ conceptos_basicos_ de_python/03-tipos_de_datos.py
@@ -0,0 +1,28 @@
+# creo cadena de caracteres tanto con "" como con ''
+nombre = 'Macarena'
+nombre
+'Macarena'
+nombre2 = "Daniel"
+# puedo concatenar
+nombre + nombre2
+'MacarenaDaniel'
+# puedo multiplicar
+nombre * 4
+'MacarenaMacarenaMacarenaMacarena'
+# mejor estetica del mensaje
+nombre + ", " + nombre2
+'Macarena, Daniel'
+# numeros decimales
+numero_decimal = 3.4
+numero_decimal
+3.4
+# creando booleanos
+es_estudiante = True
+es_estudiante
+True
+print(es_estudiante)
+True
+# creando el falso
+trabaja = False
+trabaja
+False

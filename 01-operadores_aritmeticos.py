3c74e02115bb1e324dbf09a49db5ec721d323f90
danielvinzia <danielvinzia@gmail.com>
2022-02-04 11:28:56 -0300

    operadores aritmeticos listo

---
+++ 01-operadores_aritmeticos.py
@@ -0,0 +1,20 @@
+# suma
+5 + 5
+# resta
+5 - 5
+# multiplicacion
+2 * 5
+# divison
+2 * 20
+# puedo hacer varias operaciones juntas
+5 * 4 * 3 * 2 * 1
+# division
+21 / 5
+# division que da la parte entera
+21 // 5
+# devuelve el resto de la division
+21 % 5
+# potencias
+2 ** 5
+# raiz
+144 ** 0.5
